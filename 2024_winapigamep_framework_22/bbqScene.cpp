2596c08417d2ad8d315c59d98036e9e44b350f38
02BBQ <123713362+02BBQ@users.noreply.github.com>
2024-11-20 14:14:02 +0900

    Scene update
    New Scenes!
    -Added bbq Scene
    -Added Ral Scene
    -Added Domi Scene

---
+++ 2024_winapigamep_framework_22/bbqScene.cpp
@@ -0,0 +1,35 @@
+#include "pch.h"
+#include "bbqScene.h"
+#include "Object.h"
+#include "Player.h"
+#include "InputManager.h"
+#include "SceneManager.h"
+#include "Enemy.h"
+#include "CollisionManager.h"
+#include "ResourceManager.h"
+#include "Rigidbody.h"
+void bbqScene::Init()
+{
+	Object* pObj = new Enemy;
+	pObj->SetPos({ SCREEN_WIDTH / 2.f,150.f });
+	pObj->SetSize({ 100.f,100.f });
+	pObj->SetName(L"Enemy");
+	AddObject(pObj, LAYER::ENEMY);
+
+	Object* pPlayer = new Player;
+	pPlayer->SetPos({ SCREEN_WIDTH / 2.f,500.f });
+	pPlayer->SetSize({ 100.f,100.f });
+	pPlayer->AddComponent<Rigidbody>();
+	AddObject(pPlayer, LAYER::PLAYER);
+	GET_SINGLE(CollisionManager)->CheckLayer(LAYER::PROJECTILE, LAYER::ENEMY);
+	//GET_SINGLE(CollisionManager)->CheckLayer(LAYER::PLAYER, LAYER::ENEMY);
+	GET_SINGLE(ResourceManager)->LoadSound(L"BGM", L"Sound\\Retro_bgm.wav", true);
+	GET_SINGLE(ResourceManager)->Play(L"BGM");
+}
+
+void bbqScene::Update()
+{
+	Scene::Update();
+	if (GET_KEYDOWN(KEY_TYPE::ENTER))
+		GET_SINGLE(SceneManager)->LoadScene(L"GameScene");
+}

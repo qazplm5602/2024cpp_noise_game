b594e3c1055582db4c1ab33fe347555101876f54
qazplm5602 <t01048187045@gmail.com>
2024-12-01 02:30:15 +0900

    feat: device setting

--- 2024_winapigamep_framework_22/DeviceSettingScene.cpp
+++ 2024_winapigamep_framework_22/DeviceSettingScene.cpp
@@ -1,5 +1,8 @@
 #include "pch.h"
 #include "DeviceSettingScene.h"
+#include "RectObject.h"
+#include "ImageRenderer.h"
+#include "TextRenderer.h"
 
 DeviceSettingScene::DeviceSettingScene()
 {
@@ -13,5 +16,29 @@ DeviceSettingScene::~DeviceSettingScene()
 
 void DeviceSettingScene::Init()
 {
+	// 뒤로가기
+	RectObject* backIcon = new RectObject();
+	backIcon->SetAnchor(RectAnchor::BottomLeft);
+	backIcon->SetPos(Vec2(30, 30));
+	backIcon->SetSize(Vec2(30, 30));
+	AddObject(backIcon, LAYER::UI);
+
+	backIcon->AddComponent<ImageRenderer>();
+	ImageRenderer* backImage = backIcon->GetComponent<ImageRenderer>();
+	backImage->SetTexture(L"volumeIn", L"Texture\\volumeIn.bmp");
+
+	RectObject* backTextBox = new RectObject();
+	backTextBox->SetAnchor(RectAnchor::BottomLeft);
+	backTextBox->SetPos(Vec2(30, 30));
+	backTextBox->SetSize(Vec2(30, 30));
+	AddObject(backTextBox, LAYER::UI);
+
+	backTextBox->AddComponent<TextRenderer>();
+	TextRenderer* backText = backTextBox->GetComponent<TextRenderer>();
+
+	backText->SetSize(20);
+	backText->SetText(L"뒤로가기");
+
+	// 설정하기
 
 }

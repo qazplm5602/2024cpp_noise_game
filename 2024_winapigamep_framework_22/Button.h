5b65ae32a865db9d76e046d9d0c9f2301861815b
02BBQ <123713362+02BBQ@users.noreply.github.com>
2024-12-04 15:17:44 +0900

    Delegate

--- 2024_winapigamep_framework_22/Button.h
+++ 2024_winapigamep_framework_22/Button.h
@@ -1,8 +1,20 @@
 #pragma once
 #include "RectObject.h"
+#include "Delegate.h"
 class Button :
     public RectObject
 {
-
+public:
+    Button();
+    ~Button();
+public:
+    Delegate<void> OnClick;
+    Delegate<void> OnHover;
+    Delegate<void> ExitHover;
+    virtual void Update() override;
+    void Refresh();
+private:
+    bool b_isHover;
+    RECT* rect;
 };
 

aac2f7eea06f61a04a6f9dee3187ddced0705e66
domi <domi@domi.kr>
2024-12-07 20:01:33 +0900

    asdf

--- 2024_winapigamep_framework_22/BaseWindow.cpp
+++ 2024_winapigamep_framework_22/BaseWindow.cpp
@@ -77,7 +77,7 @@ void BaseWindow::createWindow()
 
     m_hWnd = CreateWindowW(
         L"2-2 Gamep", // 윈도우 클래스 식별자
-        L"준용의 윈도우",   // 제목
+        L"Close your laptop",   // 제목
         WS_OVERLAPPEDWINDOW, // 윈도우 어떤 스타일로 만들것인가
         Winposx,  // ★ 띄울 위치의 LEFT
         Winposy,             // ★ 띄울 위치의 TOP
